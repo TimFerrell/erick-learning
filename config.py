5503445487127e108f35abd10245f3a7dc96be95
Erktec <erick@erktec.com>
2024-01-30 08:35:36 -0500

    Untracking files

--- config.py
+++
@@ -1,6 +0,0 @@
-auth_token = ("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9"
-              ".eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImIyNTliNmU2LTQ5N2EtNDUyMi04"
-              "NmVhLTVlM2FmOTEzNzc4NiIsImlhdCI6MTcwMjczNjEzMCwic3ViIjoiZGV2ZWxvcGVyLzBhM2FiMjAyLTMwMzktYTAxNi1jZG"
-              "RjLTI3ZGY3MjkwNWM2ZiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwi"
-              "dHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjcwLjEzMS40OC4yMzYiXSwidHlwZSI6ImNsaWVudCJ9XX0.i7lf-DDLS1Q"
-              "xyq4B_ui-ozvJw1VM09MS-ydmRUlJdsc2QO4gSq8zf51g7jttR2F2lhmmo92zdSYViw_8Ovih5g")
